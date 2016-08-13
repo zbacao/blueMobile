53a47985d64bacbd928abd7b55b1895b4296d064
zbacao <zbacao@qq.com>
2016-08-13 18:24:47 +0800

    8.13
    8.13

--- script/controllers/personCenter.js
+++ script/controllers/personCenter.js
@@ -2,10 +2,8 @@
  * Created by zbacao on 2016/8/11.
  */
 
-lmapp.controller("personCenter",["$scope","logo",function($scope,logo){
-        //构造一个含有目标参数的正则表达式对象
-    $scope.logo=logo.logo;
-    $scope.logo="true";
+lmapp.controller("personCenter",["$scope",function($scope){
+
 }]);
 
 
--- script/services/logo.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by zbacao on 2016/8/12.
- */
-lmapp.service("logo",function(){
-    return{
-        logo:[]
-    }
-
-});
